70ea35b3cc63ec3984e854ecd2eb7e55152b62ea
Joshua <joshua.ardin@student.umn.ac.id>
2024-05-14 21:25:32 +0700

    merge
    udah di gabungin

---
+++ src/pages/Add.tsx
@@ -0,0 +1,110 @@
+// Add.tsx
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonSelect, IonSelectOption, IonButton, IonGrid, IonRow, IonCol, IonDatetime } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+
+const Add: React.FC = () => {
+    const [amount, setAmount] = useState<number | undefined>();
+    const [selectedCategory, setSelectedCategory] = useState<string>('');
+    const [selectedType, setSelectedType] = useState<string>('');
+    const [transactionDate, setTransactionDate] = useState<string>(new Date().toISOString());
+    const [accountBalance, setAccountBalance] = useState<number>(0); // Track account balance
+    const history = useHistory();
+
+    const handleSaveTransaction = () => {
+        // Calculate new account balance based on transaction type
+        const newBalance = selectedType === 'income' ? accountBalance + (amount || 0) : accountBalance - (amount || 0);
+
+        // Update account balance
+        setAccountBalance(newBalance);
+
+        // Proses menyimpan transaksi dengan jumlah, kategori, jenis, dan tanggal yang dipilih
+        const transactionDetails = {
+            amount: amount,
+            category: selectedCategory,
+            type: selectedType,
+            date: transactionDate,
+            balance: newBalance // Include balance in transaction details
+        };
+
+        // Navigate to Home page with transaction details and updated balance
+        history.push({
+            pathname: '/tabs/home',
+            state: { transactionDetails: transactionDetails, accountBalance: newBalance }
+        });
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar color="secondary">
+                    <IonTitle  color="primary">Add Transaction</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonItem>
+                    <IonLabel color="primary" position="floating">Amount</IonLabel>
+                    <IonInput color="primary" type="number" value={amount} placeholder="Rp" onIonChange={e => setAmount(parseFloat(e.detail.value!))}></IonInput>
+                </IonItem>
+                <IonItem>
+                    <IonLabel color="primary" position="stacked">Category</IonLabel>
+                    <IonSelect  value={selectedCategory} placeholder="Select Category" onIonChange={e => setSelectedCategory(e.detail.value)}>
+                        <IonSelectOption  value="Gaji">Gaji</IonSelectOption>
+                        <IonSelectOption value="Investasi">Investasi</IonSelectOption>
+                        <IonSelectOption value="Usaha">Usaha</IonSelectOption>
+                        <IonSelectOption  value="Makanan & Minuman">Makanan & Minuman</IonSelectOption>
+                        <IonSelectOption  value="Pendidikan">Pendidikan</IonSelectOption>
+                        <IonSelectOption  value="Pajak">Pajak</IonSelectOption>
+                        <IonSelectOption value="Kendaraan">Kendaraan</IonSelectOption>
+                    </IonSelect>
+                </IonItem>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <IonLabel color="primary" position="stacked" style={{ paddingTop: '10px' }}>Type</IonLabel>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <IonButton
+                                expand="full"
+                                color={selectedType === 'income' ? 'success' : 'medium'}
+                                onClick={() => setSelectedType('income')}
+                            >
+                                Income
+                            </IonButton>
+                        </IonCol>
+                        <IonCol>
+                            <IonButton
+                                expand="full"
+                                color={selectedType === 'expenses' ? 'danger' : 'medium'}
+                                onClick={() => setSelectedType('expenses')}
+                            >
+                                Expenses
+                            </IonButton>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+                <IonItem>
+                    <IonLabel color="primary" position="stacked">Transaction Date</IonLabel>
+                    <IonGrid>
+                        <IonRow>
+                            <IonCol size="12" className="ion-text-center">
+                                <IonDatetime
+                                    value={transactionDate}
+                                    onIonChange={(e) => setTransactionDate(Array.isArray(e.detail.value) ? e.detail.value[0] : e.detail.value!)}
+                                    display-format="DD/MM/YYYY"
+                                ></IonDatetime>
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                </IonItem>
+
+                <IonButton color="tertiary" expand="full" onClick={handleSaveTransaction}><IonLabel color="light">Save Transaction</IonLabel></IonButton>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+
+export default Add;
---
+++ src/pages/NewCate.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, IonInput, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+
+const NewCate: React.FC = () => {
+    const [categoryName, setCategoryName] = useState('');
+    const [categoryType, setCategoryType] = useState('');
+    const history = useHistory();
+
+    const handleSaveCategory = () => {
+        const newCategory = { name: categoryName, type: categoryType };
+        const existingCategories = JSON.parse(localStorage.getItem('categories') || '[]');
+        const updatedCategories = [...existingCategories, newCategory];
+        localStorage.setItem('categories', JSON.stringify(updatedCategories));
+
+        history.push("/tabs/category");
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar color="secondary">
+                    <IonTitle color="primary">New Category</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+                <IonItem lines="full">
+                    <IonLabel color="primary" position="floating">Nama Kategori</IonLabel>
+                    <IonInput color="primary" value={categoryName} onIonChange={e => setCategoryName(e.detail.value!)}></IonInput>
+                </IonItem>
+                <IonItem>
+                    <IonLabel color="primary">Tipe Kategori</IonLabel>
+                </IonItem>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <IonButton expand="block" color={categoryType === 'income' ? 'success' : 'medium'} onClick={() => setCategoryType('income')}>
+                                Income
+                            </IonButton>
+                        </IonCol>
+                        <IonCol>
+                            <IonButton expand="block" color={categoryType === 'expenses' ? 'danger' : 'medium'} onClick={() => setCategoryType('expenses')}>
+                                Expense
+                            </IonButton>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+                <IonButton color="tertiary" expand="full" onClick={handleSaveCategory}><IonLabel color="light">Save</IonLabel></IonButton>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default NewCate;
